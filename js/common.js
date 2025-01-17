92fd746f58b42290680089783ca09d736bee83e7
Github ujunghee <GitHub zmfozj4172@naver.com>
2025-01-17 17:40:31 +0900

    메뉴 반응형 추가

--- js/common.js
+++ js/common.js
@@ -1,15 +1,21 @@
 
 let cursor = document.querySelector('.cursor-item')
 const cursorWidth = cursor.offsetWidth
-const coursorHeight = cursor.offsetHeight
+const cursorHeight = cursor.offsetHeight
 
 document.addEventListener('mousemove', (e) => {
     // console.log(e.clientX, e.clientY);
     const width = cursorWidth / 2
-    const height = coursorHeight / 2
+    const height = cursorHeight / 2
     
     cursor.style.transform = `translate(${e.clientX - width}px, ${e.clientY - height}px) scale(${cursor.classList.contains('hover') ? 2 : 1})`
+
+    callback();
 })
+
+function callback() {
+    window.requestAnimationFrame(callback);
+}
     
 const buttons = document.querySelectorAll('button , a, span')
 
